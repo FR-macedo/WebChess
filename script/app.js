e5f89a1f9eb3354fd2c4fda7ab723a259fe89d16
Francisco Macedo <macedo270701@gmail.com>
2024-09-28 11:01:29 -0300

    correção do ultimo commit
    por algum motivo meu gti anda ignorando a pasta script no git add

--- script/app.js
+++ script/app.js
@@ -1,9 +1,84 @@
-const gameBoard = document.querySelector("#gameboard");
+const gameBoard = document.querySelector("#gameBoard");
 const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#info-display");
 const width = 8;
 
-
 const startPieces = [
+  blackRook,
+  blackKnight,
+  blackBishop,
+  blackQueen,
+  blackKing,
+  blackBishop,
+  blackKnight,
+  blackRook,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  blackPawn,
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whitePawn,
+  whiteRook,
+  whiteKnight,
+  whiteBishop,
+  whiteQueen,
+  whiteKing,
+  whiteBishop,
+  whiteKnight,
+  whiteRook,
+];
 
-]
+function createBoard() {
+  startPieces.forEach((startPiece, i) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.innerHTML = startPiece;
+    square.setAttribute("Square-id", i);
+    square.classList.add("beige");
+    gameBoard.append(square);
+  });
+}
+
+createBoard();

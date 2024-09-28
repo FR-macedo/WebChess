272d4868be6a8b2eef57edeff65e23a7c7d20d42
Francisco Macedo <macedo270701@gmail.com>
2024-09-28 20:24:58 -0300

    atualização para a participação dos outros membros

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # TerminalChess
 ### Codando o xadrez para ser jogado no terminal 
 
-Colabore na criação do nosso jogo de xadrez. Crie um fork desse repositório e faça sugestões de alteração.
+Colabore na criação do nosso jogo de xadrez. Crie um fork desse repositório e faça sugestões de alterações!
+
+### Como funciona o xadrez?
+
--- script/app.js
+++ script/app.js
@@ -75,10 +75,34 @@ function createBoard() {
     const square = document.createElement("div");
     square.classList.add("square");
     square.innerHTML = startPiece;
+    square.firstChild?.setAttribute("draggable", true);
     square.setAttribute("Square-id", i);
-    square.classList.add("beige");
+    // square.classList.add("beige");
+
+    //colorindo os quadrados
+    const row = Math.floor((63 - i) / 8) + 1;
+    if (row % 2 === 0) {
+      square.classList.add(i % 2 === 0 ? "beige" : "brown");
+    } else {
+      square.classList.add(i % 2 === 0 ? "brown" : "beige");
+    }
+
+    if (i <= 15) {
+    } else {
+    }
+
     gameBoard.append(square);
   });
 }
 
 createBoard();
+
+const allSquares = document.querySelectorAll("#gameBoard .square");
+
+allSquares.forEach((square) => {
+  square.addEventListener("dragstart", dragStart);
+});
+
+function dragStart(e) {
+  console.log(e);
+}

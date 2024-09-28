b6b3970516c3410efa516d797385cb1b9d659322
Francisco Macedo <macedo270701@gmail.com>
2024-09-28 10:07:12 -0300

    nova tentativa de dar o commit dos icones

---
+++ script/app.js
@@ -0,0 +1,9 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+
+
+const startPieces = [
+
+]
